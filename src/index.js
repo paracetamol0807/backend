f1e21c7d366d244aa143599642b43afc057e8296
Paracetamol <paracetamol.codingpractice@gmail.com>
2026-02-22 01:40:33 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config({
-  path : './env'
-})
-
-
-
-
+  path: "./env",
+});
 
 connectDB()
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !! ", err);
+  });
 
 /*
 
